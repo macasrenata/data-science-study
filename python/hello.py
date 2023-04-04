0750be2f52dd2985185b7fda8816ad4a6ed1911e
macasrenata <renata.macas@gmail.com>
2023-04-03 21:12:11 -0300

    start learning

---
+++ python/hello.py
@@ -0,0 +1,20 @@
+# // mostrar resultado
+print("hello world")
+
+# // imprimir resultado int
+print(50)
+
+# // imprimir resultado int, string, float...
+print(50, 1000, 3.142, "Hello World")
+
+# // imprimir resultado int, string, float... com separador
+print("Hello", end="")
+print("World")
+
+# // imprimir resultado int, string, float... com separador
+print("Hello", end=" ")
+print("World")
+
+""" Docstrings are pretty cool
+for writing longer comments
+or notes about the code"""
